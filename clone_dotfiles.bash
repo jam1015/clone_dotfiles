c3c0820f007d8210be4104c6ba470078c8785469
Jordan Mandel <jordan.mandel@live.com>
2023-04-13 15:13:15 +0000

    added sparse checkout

--- clone_dotfiles.bash
+++ clone_dotfiles.bash
@@ -1,8 +1,14 @@
 #/bin/bash
 
-REPO="https://github.com/jam1015/dotfiles"
+# clones my dotfiles in a bare repo.  cd to home directory after and run `dots checkout` to actually copy the dotfiles
+# use the `source` command on this file so that the alias is applied to the shell we are actually using
+REPO="git@github.com:jam1015/dotfiles"
 cd $HOME
 git clone --bare $REPO
 alias dots='/usr/bin/git --git-dir=$HOME/dotfiles.git --work-tree=$HOME'
-dots config --local status.showUntrackedFiles no
+/usr/bin/git --git-dir=$HOME/dotfiles.git --work-tree=$HOME config --local status.showUntrackedFiles no
+touch $HOME/dotfiles.git/info/sparse-checkout
+echo -e "/*\nREADME.md" > $HOME/dotfiles.git/info/sparse-checkout
+/usr/bin/git --git-dir=$HOME/dotfiles.git --work-tree=$HOME config --local core.sparsecheckout true
+cd -
 #dots checkout
